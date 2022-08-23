32129225a9f404ad2f60abbfac71fa4c701d64a3
davidvazgon26 <davidvazgon@hotmail.com>
2022-08-23 00:55:52 -0500

    Initial commit

---
+++ src/main/java/com/cursojava/cursoDemo/CursoDemoApplication.java
@@ -0,0 +1,13 @@
+package com.cursojava.cursoDemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CursoDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CursoDemoApplication.class, args);
+	}
+		//	System.out.println("Hola");
+}
---
+++ src/test/java/com/cursojava/cursoDemo/CursoDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cursojava.cursoDemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CursoDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
